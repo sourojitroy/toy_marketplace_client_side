143b7ff73a1c81de553b4642a7750bc387888628
sourojitroy <roy.souro2013@gmail.com>
2023-05-21 22:09:29 +0600

    PrivateRoute file added

---
+++ src/Routes/PrivateRourte.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const PrivateRourte = ({children}) => {
+    const {user,loading} = useContext(AuthContext)
+    const location= useLocation();
+
+    if(loading){
+        return <progress className="progress w-56"></progress>;
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate state={{from: location}} to='/login' replace></Navigate>
+};
+
+export default PrivateRourte;
